ca8dea58519b2bdd01ab03138cbc4d0ee8aa44c8
Rakshitha <r3836040@gmail.com>
2026-09-04 22:33:04 +0530

    register

--- Basics.py
+++ Basics.py
@@ -1,6 +1,6 @@
 a=input("Enter your name:")
 b=input("Enter your USN:")
 c=int(input("Enter the mobile number:"))
-skills=["python","DSA","SQL"]
+skills=["python","DSA","SQL","Raksh"]
 for skill in skills:
     print(skill)
