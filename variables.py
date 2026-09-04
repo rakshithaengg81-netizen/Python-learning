0fd305d9338b3da74377000d6557c5415e563541
Rakshitha <r3836040@gmail.com>
2026-09-04 22:30:50 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,17 @@
+name = "Rakshitha.H.M" #string
+age = 20 #integer
+is_student = True  #Boolean
+College = "Alva's institute of engineering and technology"  #string
+CGPA = 9.24  #float
+
+print("name=" ,name)
+print("age=",age)
+print("she is a student right:",is_student)
+print("college name=",College)
+print("CGPA=",CGPA)
+
+print(type(name))
+print(type(age))
+print(type(is_student))
+print(type(College))
+print(type(CGPA))
