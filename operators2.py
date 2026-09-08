c78b28c369314d4d37d51433e52240ffb14d4bce
Rakshitha <r3836040@gmail.com>
2026-09-08 22:38:51 +0530

    operatore2

---
+++ operators2.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+#if a<b:
+    #print("a is less than b")
+#elif b<a:
+    #print("b is less than a")
+#elif a==b:
+    #print("both a and b equal")
+if a<=b:
+    print("a is less than or equal to b")
+elif a>=b:
+    print("a is more than or equal to b" )
+elif a!=b:
+    print("a not equal to b")
