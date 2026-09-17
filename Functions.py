f8030da1dff37fc4bf34ef1d2bd0ed9c2ee1df9d
Rakshitha <r3836040@gmail.com>
2026-09-17 06:58:44 +0530

    Functions

---
+++ Functions.py
@@ -0,0 +1,29 @@
+def age(age):
+    print("Your age is:",age)
+age(20)
+
+def sum(a,b):
+    print("Addition of a and b:" ,a+b)
+sum(5,5)
+
+def greet(name,age):
+    print("My name is" ,name)
+    print("And my age is" ,age)
+greet("Rakshitha",20)
+
+def checknumber(n):
+    if n%2==0:
+        print("Number is even")
+    else:
+        print("Number is odd")
+checknumber(9)
+
+def multiply(x,y):
+    a=x*y
+    print("Answer of multiply x and y:",a)
+multiply(5,20)
+
+def square(p):
+    a=p ** 2
+    print("Square of p is:",a)
+square(25)
